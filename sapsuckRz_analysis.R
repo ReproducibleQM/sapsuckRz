5d373e0623c53786447b076f816ad032b477ae3f
Chad Zirbel <crzirbel@gmail.com>
2017-01-17 08:38:56 -0500

    Initial Rscript for analysis
    Intial Rscript for analysis created. Pulling data from the LTER database
    (currently not working).

---
+++ sapsuckRz_analysis.R
@@ -0,0 +1,8 @@
+#sapsuckRz_anylsis
+
+#load required packages
+library(data.table)
+
+#load in aphid data
+#Using fread in the data.table package because this file is quite large
+aphid<-fread("http://lter.kbs.msu.edu/datatables/122.csv")
