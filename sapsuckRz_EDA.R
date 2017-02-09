6a10255c3f710648a02874419e63797f67efaf4c
crzirbel <crzirbel@gmail.com>
2017-02-09 12:06:25 -0500

    Added analysis to control for crop acreage

--- sapsuckRz_EDA.R
+++ sapsuckRz_EDA.R
@@ -2,13 +2,14 @@
 
 #load required packages
 library(data.table)
-library(data.table)
 library(reshape2)
 library(Hmisc)
 
 ##load in aphid data
 #Using fread in the data.table package because this file is quite large
 aphid<-fread("https://dl.dropboxusercontent.com/u/98197254/Mastersuction_may29_2014.csv")
+counties<-read.csv("counties+coordinates.csv")
+crop<-read.csv("counties+acres.csv")
 
 ##Pull wheat pests
 #list of wheat aphids
@@ -24,7 +25,7 @@ aphid.cut<-aphid[aphid$Aphid.Species%in%aphid.names.cut]
 aphid.cut$time<-((aphid.cut$Year-2005)*365)+aphid.cut$Date
 
 #reshape to wide
-aphid.cast<-dcast(aphid.cut,time+Site~Aphid.Species,value.var="Captures",sum)
+aphid.cast<-dcast(aphid.cut,time+Site+Year~Aphid.Species,value.var="Captures",sum)
 
 #corelation between species
 rcorr(as.matrix(aphid.cast[3:8]))
@@ -34,6 +35,71 @@ aphid.cast.cut<-aphid.cast[aphid.cast$Rhopalosiphum.maidis!=0 & aphid.cast$Rhopa
 
 #Check cor again
 rcorr(as.matrix(aphid.cast.cut[3:8]))
+
+##control for number of acres plotted in crops for each county in each year. Then look at correlations
+#rename variables so they match
+names(counties)<-c("Site","lat","long","county","state")
+names(crop)<-c("Year","state","county","crop","item","value")
+
+#merge the trap location and crop data
+crop.county<-merge(counties,crop, by="county",all=T)
+crop.cut<-crop.county[,c(2,6,10)]
+
+#data cleaning so files merge correctly
+#change to uppercase and delete spaces
+aphid.cast$Site<-toupper(aphid.cast$Site)
+aphid.cast$Site<-gsub(" ","",aphid.cast$Site)
+crop.cut$Site<-gsub(" ","",crop.cut$Site)
+
+#fix spelling of Bean & Beet
+crop.cut$Site<-gsub("BEAT","BEET",crop.cut$Site)
+
+#merge crop and aphid data
+aphid.crop<-merge(aphid.cast,crop.cut,by=c("Site","Year"),all=T)
+aphid.crop$value<-as.numeric(aphid.crop$value)
+aphid.crop<-na.omit(aphid.crop)
+
+#models (abundance of each species predicting by amount of crop planted in that county)
+mod.dn<-lm(Diuraphis.noxia~value,data=aphid.crop)
+mod.md<-lm(Metopolophium.dirhodum~value,data=aphid.crop)
+mod.rm<-lm(Rhopalosiphum.maidis~value,data=aphid.crop)
+mod.rp<-lm(Rhopalosiphum.padi~value,data=aphid.crop)
+mod.sg<-lm(Schizaphis.graminum~value,data=aphid.crop)
+mod.sa<-lm(Sitobion.avenae~value,data=aphid.crop)
+
+#add residuals to dataframe
+aphid.crop$dn.r<-resid(mod.dn)
+aphid.crop$md.r<-resid(mod.md)
+aphid.crop$rm.r<-resid(mod.rm)
+aphid.crop$rp.r<-resid(mod.rp)
+aphid.crop$sg.r<-resid(mod.sg)
+aphid.crop$sa.r<-resid(mod.sa)
+
+#check for correlations between residuals of each of the species controling for crop acreage
+rcorr(as.matrix(aphid.crop[11:16]))
+
+#now aggregate by year (correlations on different temporal scale)
+aphid.crop.ag<-aggregate(.~Year+Site,data=aphid.crop, sum)
+rcorr(as.matrix(aphid.crop.ag[4:9]))
+
+#models (abundance of each species predicting by amount of crop planted in that county)
+mod.dn.ag<-lm(Diuraphis.noxia~value,data=aphid.crop.ag)
+mod.md.ag<-lm(Metopolophium.dirhodum~value,data=aphid.crop.ag)
+mod.rm.ag<-lm(Rhopalosiphum.maidis~value,data=aphid.crop.ag)
+mod.rp.ag<-lm(Rhopalosiphum.padi~value,data=aphid.crop.ag)
+mod.sg.ag<-lm(Schizaphis.graminum~value,data=aphid.crop.ag)
+mod.sa.ag<-lm(Sitobion.avenae~value,data=aphid.crop.ag)
+
+#add residuals to dataframe
+aphid.crop.ag$dn.r<-resid(mod.dn.ag)
+aphid.crop.ag$md.r<-resid(mod.md.ag)
+aphid.crop.ag$rm.r<-resid(mod.rm.ag)
+aphid.crop.ag$rp.r<-resid(mod.rp.ag)
+aphid.crop.ag$sg.r<-resid(mod.sg.ag)
+aphid.crop.ag$sa.r<-resid(mod.sa.ag)
+
+#look at cor between resids
+rcorr(as.matrix(aphid.crop.ag[11:16]))
 
 ##plotting
 
