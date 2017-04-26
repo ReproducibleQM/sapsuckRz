ce86ecab180784da33c8c881f72061c6f33443bc
crzirbel <crzirbel@gmail.com>
2017-04-25 21:56:36 -0400

    Adding NMDS and abundance plots for EEBB poster

--- sapsuckRz_analysis.R
+++ sapsuckRz_analysis.R
@@ -3,7 +3,18 @@
 #load required packages
 library(data.table)
 
-#load in aphid data
+#load required packages
+library(data.table)
+
+##load in aphid data!
 #Using fread in the data.table package because this file is quite large
-aphid<-fread("http://lter.kbs.msu.edu/datatables/122.csv")
-#test
+aphid<-fread("https://dl.dropboxusercontent.com/u/98197254/Mastersuction_may29_2014.csv")
+
+##Pull wheat pests
+#list of wheat aphids
+aphid.names.cut<-c("Schizaphis.graminum","Diuraphis.noxia","Sitobion.avenae","Rhopalosiphum.padi","Rhopalosiphum.maidis","Metopolophium.dirhodum")
+
+#cut down the data to only include those 10 species
+aphid.cut<-aphid[aphid$Aphid.Species%in%aphid.names.cut]
+
+write.csv(aphid.cut,"aphid_wheat.csv")
