0cf28c7fabc78c7ef368258f8437a441c6864ffa
crzirbel <crzirbel@gmail.com>
2017-12-09 11:25:35 -0500

    updating weather data and major cleaning overhaul.

--- sapsuckRz_analysis.R
+++ sapsuckRz_analysis.R
@@ -11,6 +11,7 @@ library(lme4)
 library(r2glmm)
 library(car)
 library(mgcv)
+library(zoo)
 source("custom_functions.r")
 
 ##load in aphid data
@@ -18,118 +19,6 @@ source("custom_functions.r")
 aphid<-fread("https://ndownloader.figshare.com/files/9465745")
 aphid<-aphid[,-1]
 colnames(aphid)<-c("Year","State","Site","Date","Sex","Species","Captures")
-
-##Pull the 10 most common species
-#find the average number of captures for each species
-aphid.ag<-aggregate(Captures~Aphid.Species,data=aphid,sum)
-#order data from most number of captures to least
-aphid.ord<-aphid.ag[order(-aphid.ag$Captures),] 
-#make a list of the 10 most common species
-aphid.names.cut<-aphid.ord$Aphid.Species[1:25]
-#cut down the data to only include those 10 species
-aphid.cut<-aphid[aphid$Aphid.Species%in%aphid.names.cut]
-
-#add a (crude) time variable
-aphid.cut$time<-((aphid.cut$Year-2005)*365)+aphid.cut$Date
-
-####NMDS####
-aphid.cast<-dcast(aphid.cut,Year+State~Aphid.Species,value.var="Captures",sum)
-rownames(aphid.cast)<-paste(aphid.cast$State,aphid.cast$Year,sep="_")
-aphid.year<-as.factor(aphid.cast$Year)
-aphid.state<-as.factor(aphid.cast$State)
-aphid.cast$Year<-NULL
-aphid.cast$State<-NULL
-
-nmds.aphid<-metaMDS(aphid.cast,k=2,trymax=250,distance="bray")
-
-adonis(vegdist(aphid.cast)~aphid.year,permutations=10000)
-sim.year<-simper(aphid.cast,aphid.year)
-summary(sim.year,ordered=TRUE) #What's driving this? looks like 2005
-
-adonis(vegdist(aphid.cast)~aphid.state,permutations=10000)
-sim.state<-simper(aphid.cast,aphid.state)
-summary(sim.state,ordered=TRUE) #What's driving this? Looks like Iowa
-
-##plotting
-tiff("Aphid_NMDS_year.tiff",width = 3200, height = 3200, units = "px", res = 800)
-par(mar=c(5.1, 4.1, 4.1, 8.1), xpd=TRUE)
-ordiplot(nmds.aphid,type="n",main="Aphid composition by year")
-points(nmds.aphid,display="sites",pch=16,cex=1.25,col=c("#e41a1c","lightblue","#4daf4a","#984ea3","#ff7f00","darkblue","#a65628","#f781bf","#999999")[aphid.year])
-#ordiellipse(nmds.aphid, aphid.year ,col=c("#e41a1c","lightblue","#4daf4a","#984ea3","##ff7f00","darkblue","#a65628","#f781bf","#999999"))
-legend(x=.85,y=1,expression("2005","2006","2007","2008","2009","2010","2011","2012","2013")
-       ,pch=16,col=c("#e41a1c","lightblue","#4daf4a","#984ea3","#ff7f00","darkblue","#a65628","#f781bf","#999999"),cex=.95)
-dev.off()
-
-tiff("Aphid_NMDS_state.tiff",width = 3200, height = 3200, units = "px", res = 800)
-par(mar=c(5.1, 4.1, 4.1, 8.1), xpd=TRUE)
-ordiplot(nmds.aphid,type="n",main="Aphid composition by state")
-points(nmds.aphid,display="sites",pch=16,cex=1.25,col=c("#e41a1c","lightblue","#4daf4a","#984ea3","#ff7f00","darkblue","#a65628","#f781bf","#999999","black")[aphid.state])
-legend(x=.85,y=1,expression("Iowa","Illinois","Indiana","Michigan","Minnesota","Wisconsin","Kansas","Kentucky","Missouri","South Dakota")
-       ,pch=16,col=c("#e41a1c","lightblue","#4daf4a","#984ea3","#ff7f00","darkblue","#a65628","#f781bf","#999999","black"),cex=.85)
-dev.off()
-
-#Number of captures through time across all sites
-ggplot(aphid.cut,aes(time,Captures,colour=Aphid.Species))+
-  geom_line(size=1)+
-  annotate("segment", x=-Inf, xend=Inf, y=-Inf, yend=-Inf,size=1)+
-  annotate("segment", x=-Inf, xend=-Inf, y=-Inf, yend=Inf,size=1)+
-  theme(axis.line.x = element_line(color = "black",size=1), axis.line.y = element_line(color = "black",size=1),
-        text = element_text(size=14),axis.text=element_text(colour="black"),
-        panel.background=element_blank(),panel.grid.major=element_blank(),panel.grid.minor=element_blank(),
-        axis.line = element_line(size=.7, color="black"),
-        strip.background = element_rect(colour="white", fill="white"),legend.position="top")
-
-aphid.w.aphgly<-aphid.cut[aphid.cut$Aphid.Species=="Aphis.glycines"]
-#facet for each state
-p1<-ggplot(aphid.w.aphgly,aes(Date,Captures,colour=Aphid.Species))+
-  geom_smooth(size=1.25,method="gam",formula=y ~ poly(x, 10),se=F)+
-  facet_wrap(~Year)+
-  labs(x="Day of Year", y="Aphid Captures")+
-  annotate("segment", x=-Inf, xend=Inf, y=-Inf, yend=-Inf,size=1)+
-  annotate("segment", x=-Inf, xend=-Inf, y=-Inf, yend=Inf,size=1)+
-  coord_cartesian(ylim = c(0, 500))+
-  theme(axis.line.x = element_line(color = "black",size=1), axis.line.y = element_line(color = "black",size=1),
-        text = element_text(size=14),axis.text=element_text(colour="black"),
-        panel.background=element_blank(),panel.grid.major=element_blank(),panel.grid.minor=element_blank(),
-        axis.line = element_line(size=.7, color="black"),
-        strip.background = element_rect(colour="white", fill="white"),legend.position="top")
-
-tiff("Aphid_abund_year_soy.tiff",width = 3200, height = 3200, units = "px", res = 800)
-p1
-dev.off()
-
-#Remove soybean aphid and replot (easier to see variation in other species)
-aphid.cut.aphgly<-aphid.cut[aphid.cut$Aphid.Species!="Aphis.glycines"]
-
-#soybean aphid removed
-p2<-ggplot(aphid.cut.aphgly,aes(Date,Captures,colour=Aphid.Species))+
-  geom_smooth(size=1.25,method="gam",formula=y ~ poly(x, 10),se=F)+
-  facet_wrap(~Year)+
-  labs(x="Day of Year", y="Aphid Captures")+
-  annotate("segment", x=-Inf, xend=Inf, y=-Inf, yend=-Inf,size=1)+
-  annotate("segment", x=-Inf, xend=-Inf, y=-Inf, yend=Inf,size=1)+
-  coord_cartesian(ylim = c(0, 100))+
-  scale_colour_manual(values=c("#e41a1c","lightblue","#4daf4a","#984ea3","#ff7f00","darkblue","#a65628","#f781bf","#999999"))+
-  theme(axis.line.x = element_line(color = "black",size=1), axis.line.y = element_line(color = "black",size=1),
-        text = element_text(size=14),axis.text=element_text(colour="black"),
-        panel.background=element_blank(),panel.grid.major=element_blank(),panel.grid.minor=element_blank(),
-        axis.line = element_line(size=.7, color="black"),
-        strip.background = element_rect(colour="white", fill="white"),legend.position="top")
-
-tiff("Aphid_abund_year.tiff",width = 8000, height = 4000, units = "px", res = 600)
-p2
-dev.off()
-#facet for each state with soybean aphid removed
-ggplot(aphid.cut.aphgly,aes(time,Captures,colour=Aphid.Species))+
-  geom_smooth(method="gam",size=1)+
-  facet_wrap(~State)+
-  annotate("segment", x=-Inf, xend=Inf, y=-Inf, yend=-Inf,size=1)+
-  annotate("segment", x=-Inf, xend=-Inf, y=-Inf, yend=Inf,size=1)+
-  theme(axis.line.x = element_line(color = "black",size=1), axis.line.y = element_line(color = "black",size=1),
-        text = element_text(size=14),axis.text=element_text(colour="black"),
-        panel.background=element_blank(),panel.grid.major=element_blank(),panel.grid.minor=element_blank(),
-        axis.line = element_line(size=.7, color="black"),
-        strip.background = element_rect(colour="white", fill="white"),legend.position="top")
 
 ##Looking for phenology change
 aphid.max<-as.data.frame(aphid.cut %>% group_by(Year,Site,Aphid.Species) %>% slice(which.max(Captures)))
@@ -169,9 +58,6 @@ weather.cast$dd<-allen(weather.cast$max.temp, weather.cast$min.temp, 10.000001)
 #starting March 1
 weather.cast$dd.acum<-accum.allen(weather.cast$max.temp,weather.cast$min.temp,10.000001,weather.cast$doy,60)
 
-#getting 8 warnings. look for misbehaving rows
-weather.cast[c(which(is.na(weather.cast), arr.ind=TRUE)[,1]),]
-
 ##precipitation data
 #precipitation accumulation over the growing season
 #starting March 1
@@ -181,70 +67,24 @@ weather.cast$precip.accum<-accum.precip.time(weather.cast$precip,weather.cast$do
 names(aphid.all)<-c("year","doy","site.id","captures")#so they match
 weather.cast$year<-isoyear(weather.cast$date)
 
-data<-merge(aphid.all,weather.cast,by=c("site.id","year","doy"),all.x=T)
+data<-merge(aphid.all,weather.cast,by=c("site.id","year","doy"),all=T)
+
+#Calculate rolling averages degree day accumulation
+data$dd.week<-rollmean(data$dd, 7, align = "right", fill = NA)
+data$dd.month<-rollmean(data$dd, 30, align = "right", fill = NA)
+data$dd.year<-data$dd.acum/data$doy
+
+#Calculate rolling average precipitation
+data$precip.week<-rollmean(data$precip, 7, align = "right", fill = NA)
+data$precip.month<-rollmean(data$precip, 30, align = "right", fill = NA)
+data$precip.year<-data$precip.accum/data$doy
+
+#Calculate total precipitation before peak
+data$precip.week.tot<-rollsum(data$precip, 7, align = "right", fill = NA)
+data$precip.month.tot<-rollsum(data$precip, 30, align = "right", fill = NA)
 
 ##Start building models##
 
-#fit quadratic term
-#data$dd.acum2<-(data$dd.acum)^2
-
-#fit model
-aphid.model<- lm(captures~dd.acum+I(dd.acum^2),data=data[which(data$year==2005 & data$site.id=="ACRE"),])
-#generate range of 50 numbers starting from 30 and ending at 160
-xx <- seq(6,2000, length=50)
-plot(captures~dd.acum,pch=19,ylim=c(0,500),data=data[which(data$year==2005 & data$site.id=="ACRE"),])
-lines(xx, predict(aphid.model, data.frame(dd.acum=xx)), col="red",lwd=4)
-
-##
-data$year<-as.factor(data$year)
-
-##remove observations before April 15 and after September 30
-data<-data[which(data$doy>=105 | data$doy<=273),]
-
-##remove years and sites with less than 6 observations
-data<-data %>% group_by(site.id,year) %>% filter(n()>= 7) %>% ungroup()
-data<-data.table(data)#use data.table class
-
-#peak function
-argmax <- function(x, y, w=3, ...) {
-  require(zoo)
-  n <- length(y)
-  y.smooth <- loess(y ~ x, span=.5,...)$fitted
-  y.max <- rollapply(zoo(y.smooth), 2*w+1, max, align="center")
-  delta <- y.max - y.smooth[-c(1:w, n+1-1:w)]
-  i.max <- which(delta <= 0) + w
-  list(x=x[i.max], i=i.max, y.hat=y.smooth)
-}
-
-##calculate peaks
-peaks<- data[,list(peak=argmax(.SD$dd.acum,.SD$captures)$x),by=c("site.id","year")]
-#Some site x years have multiple peaks. Need to work on this.
-
-##add rainfall data
-
-#find the week each peak was in
-data$week<-isoweek(data$date)
-
-weeks<-c()
-for (i in 1:length(peaks$year)){
-  #pull out data for that year
-  year.subset<-data[which(data$year==peaks$year[i]),]
-  #pull out data for that site
-  year.site.subset<-year.subset[which(year.subset$site.id==peaks$site.id[i]),]
-  #ditch rows that occur after the peak DD accum
-  over.dd.subset<-year.site.subset[which(year.site.subset$dd.acum<peaks$peak[i]),]
-  #the last week in the data is the week the peak occurs in
-  weeki<-max(over.dd.subset$week)
-  #concatenate this value to the weeks vector
-  weeks<-c(weeks, weeki)
-}
-peaks$week<-weeks
-
-#put it into our peak object
-peaks$week<-weeks
-
-#merge peaks and data file
-data<-merge(data,peaks,by=c("site.id","year","week"),all=T)
 
 ##load in landcover data##
 cdl<-read.csv("Data/cdl.csv")
